86bdd470929d310e43a674c1af5a35dd29134cba
Josh-A-Perez <23joshperez30@gmail.com>
2021-05-20 11:42:13 -0700

    flexible columns

---
+++ MourningAlgo/week1Day4.js
@@ -0,0 +1,13 @@
+// create a loop
+// set var i to 1
+//if var i <= 100 add 1 to i
+// console log i
+// if the sum is divisible by 3 console log fizz
+// if i is divisible by 5 console log buzz
+// else if i is divisible by 3 and 5 console log fizzbuzz
+
+for(var i; i <=100; i++){
+    console.log (i)
+    
+
+}
