adf7fdb2a97fb350ca4e4c610e92b24ff0721ef6
Josh-A-Perez <23joshperez30@gmail.com>
2021-05-27 23:18:04 -0700

    Dojo Weather

---
+++ DocumentObjects/DojoWeather/Script.js
@@ -0,0 +1,13 @@
+console.log("whats the weather like?")
+
+function hidediv() {
+    var x = document.getElementById("pop-up");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+////////////////////////////////////////////////
+
